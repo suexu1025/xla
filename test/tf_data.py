d4d15ccea6b015c162798caa90a5f917a95a1ca0
Qinwen Xu <suexu1025@gmail.com>
2023-04-13 21:35:50 +0000

    add permute

--- test/tf_data.py
+++ test/tf_data.py
@@ -23,8 +23,7 @@ class TorchConvertDataLoader:
 
 def scale(image, label):
     image = tf.cast(image, tf.float32)
-    image = tf.expand_dims(
-    image, axis = 0)
+    image = tf.transpose(image, perm = [2, 0, 1])
     image /= 255
     mean = 0.1307
     std = 0.3081
